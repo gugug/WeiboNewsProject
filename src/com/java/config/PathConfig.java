e0aecde07ffa6883c91998f551b2e083860f3502
gugug <734093894@qq.com>
2017-08-20 20:02:29 +0800

    增加路径配置文件

---
+++ src/com/java/config/PathConfig.java
@@ -0,0 +1,12 @@
+package com.java.config;
+
+public class PathConfig {
+
+	public static final String ROOT_PATH = "D:/javaEE/workspace/WeiboNewsProject/WebContent/";
+	public static final String TOPIC_JSON_PATH = "Documents/TopicJson/";
+	public static final String TOPIC_XML_PATH = "Documents/TopicXML/";
+	public static final String TRANSFORM_JSON_PATH = "Documents/TransformJson/";
+	public static final String CHARACTER_TXT_PATH = "Documents/CharacterTXT/";
+	public static final String TRANSFORM_TXT_PATH = "Documents/TransformTXT/";
+
+}
